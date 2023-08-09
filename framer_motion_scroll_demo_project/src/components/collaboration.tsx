edb2b583d147b26c0fc9461eab6b34f70f57dc3c
awsk1994 <awsk1994@gmail.com>
2023-08-09 22:14:34 +0800

    refix collaboration based on youtube tutorial

--- framer_motion_scroll_demo_project/src/components/collaboration.tsx
+++ framer_motion_scroll_demo_project/src/components/collaboration.tsx
@@ -1,20 +1,38 @@
-import { motion } from "framer-motion";
+import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 export const Collaboration = () => {
-  return (
-    <section className="relative z-10 mt-[-30vh] h-[300vh]">
-      <div className="mb-[-120vh] h-[420vh] w-full">
-        <div className="sticky top-[10vh]">
-          <div className="flex justify-center">
-            <motion.div className="origin-top">
-              <motion.img
-                src="/main-screen.svg"
-                className="h-auto max-h-none w-[70vw]"
-              />
-            </motion.div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    const targetRef = useRef<HTMLDivElement | null>(null);
+
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+        offset: ["end end", "end start"],
+    });
+    const scale = useTransform(
+        scrollYProgress,
+        [0.1, 0.4],
+        [1, 2.5]
+    );
+    const x = useTransform(
+        scrollYProgress,
+        [0.1, 0.25, 0.7, 1],
+        ["0%", "-55%", "-60%", "-80%"]
+    );
+
+    return (
+        <section className="mt-[-30vh]">
+            <div ref={targetRef} className="h-[300vh] w-full">
+                <div className="sticky top-[10vh]">
+                    <div className="flex justify-center">
+                        <motion.div style={{x, scale}} className="origin-top">
+                            <img
+                                src="/main-screen.svg"
+                                className="h-[75vh] max-h-[45w] w-auto"
+                            />
+                        </motion.div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
